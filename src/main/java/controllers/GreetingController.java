9f5f7ea8b865855bc426788efb703a071548c8a9
vitalii-lototckii <vloto@softserveinc.com>
2018-03-20 16:27:55 +0200

    Created a simple REST application

---
+++ src/main/java/controllers/GreetingController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import root1.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String value) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, value));
+    }
+}
